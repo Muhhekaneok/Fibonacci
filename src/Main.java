8caeab4c94a8246d894e97eb465339a5339d7ab7
Muhhekaneok <93113207+Muhhekaneok@users.noreply.github.com>
2022-12-23 00:44:23 +0600

    Fibonacci by variables only

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        printFibonacciNumber(10);
+    }
+
+    public static void printFibonacciNumber(int digit) {
+        int f0 = 0;
+        int f1 = 1;
+        System.out.print(f0 + " " + f1 + " ");
+        for (int i = 0; i < digit; i++) {
+            int fFibo = f0 + f1;
+            System.out.print(fFibo + " ");
+            f0 = f1;
+            f1 = fFibo;
+        }
+    }
+}
